c02dcf7863dde537e075a7d624f8bcd0e190eb33
Xeraph <jeongdo.dev@gmail.com>
2023-11-18 03:46:43 +0900

    remove: remove keyframes, css'

--- src/layout/Header/style.ts
+++ src/layout/Header/style.ts
@@ -1,4 +1,4 @@
-import styled, { css, keyframes } from "styled-components";
+import styled from "styled-components";
 import { THEME } from "../../style/theme";
 import { DEVICE } from "../../style/media";
 
